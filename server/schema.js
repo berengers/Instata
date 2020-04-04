65da81e4bdd4ed6d269c957ed08c701cf15a08da
Berenger Salmon <bsalmon@kaliop.com>
2020-04-04 21:59:05 +0200

    Feature - Pagination with cursor for posts query - server side

--- server/schema.js
+++ server/schema.js
@@ -1,96 +1,112 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  ########## SCALAR ###########
-  scalar Date
+	########## SCALAR ###########
+	scalar Date
 
-  ########## TYPE ###########
+	########## TYPE ###########
 
-  type FeedCursor {
-    postDate: Date!
-    postId: ID!
-  }
+	type FeedCursor {
+		postDate: Date!
+		postId: ID!
+	}
 
-  type FeedResponse {
-    cursor: FeedCursor
-    hasMore: Boolean
-    posts: [Post]!
-  }
+	type FeedResponse {
+		cursor: FeedCursor
+		hasMore: Boolean
+		posts: [Post]!
+	}
 
-  type Like {
-    id: ID!
-    user: PublicUser!
-    post: Post!
-    createdAt: Date!
-    updatedAt: Date!
-  }
+	type Like {
+		id: ID!
+		user: PublicUser!
+		post: Post!
+		createdAt: Date!
+		updatedAt: Date!
+	}
 
-  type Login {
-    token: String!
-    user: PublicUser!
-  }
+	type Login {
+		token: String!
+		user: PublicUser!
+	}
 
-  type Post {
-    id: ID!
-    description: String
-    media: String!
-    liked: Boolean!
-    likes: [Like]!
-    likesCount: Int!
-    user: PublicUser!
-    createdAt: Date!
-    updatedAt: Date!
-  }
+	type Post {
+		id: ID!
+		description: String
+		media: String!
+		liked: Boolean!
+		likes: [Like]!
+		likesCount: Int!
+		user: PublicUser!
+		createdAt: Date!
+		updatedAt: Date!
+	}
 
-  type PublicUser {
-    id: ID!
-    username: String!
-    name: String!
-    description: String
-    profilePicture: String
-    isFollowed: Boolean
-    follows: [PublicUser]!
-    followsCount: Int!
-    followers: [PublicUser]!
-    followersCount: Int!
-    posts: [Post]!
-    postsCount: Int!
-    createdAt: Date!
-    updatedAt: Date!
-  }
+	type PostsCursor {
+		postId: ID!
+		postDate: Date!
+	}
 
-  ########## INPUT ###########
+	type PostsResponse {
+		cursor: PostsCursor!
+		hasMore: Boolean!
+		posts: [Post]!
+	}
 
-  input PostInput {
-    description: String
-    media: String!
-    createdAt: Date
-    updatedAt: Date
-  }
+	type PublicUser {
+		id: ID!
+		username: String!
+		name: String!
+		description: String
+		profilePicture: String
+		isFollowed: Boolean
+		follows: [PublicUser]!
+		followsCount: Int!
+		followers: [PublicUser]!
+		followersCount: Int!
+		posts(userId: ID, cursor: PostCursorInput, limit: Int): PostsResponse!
+		postsCount: Int!
+		createdAt: Date!
+		updatedAt: Date!
+	}
 
-  input FeedCursorInput {
-    postDate: Date!
-    postId: ID!
-  }
+	########## INPUT ###########
 
-  ########## QUERY ###########
+	input PostCursorInput {
+		postId: ID!
+		postDate: Date!
+	}
 
-  type Query {
-    user(id: ID, username: String): PublicUser!
-    post(id: ID!): Post!
-    posts(userId: ID): [Post]!
-    feed(limit: Int, cursor: FeedCursorInput): FeedResponse!
-  }
+	input PostInput {
+		description: String
+		media: String!
+		createdAt: Date
+		updatedAt: Date
+	}
 
-  ########## MUTATION ###########
+	input FeedCursorInput {
+		postDate: Date!
+		postId: ID!
+	}
 
-  type Mutation {
-    toggleFollow(userId: ID!): PublicUser!
-    createPost(post: PostInput!): Post!
-    togglePostLike(postId: ID!): Post!
-    login(email: String!, password: String!): Login!
-    logout(token: String!): Boolean
-  }
+	########## QUERY ###########
+
+	type Query {
+		user(id: ID, username: String): PublicUser!
+		post(id: ID!): Post!
+		posts(userId: ID, cursor: PostCursorInput, limit: Int): PostsResponse!
+		feed(limit: Int, cursor: FeedCursorInput): FeedResponse!
+	}
+
+	########## MUTATION ###########
+
+	type Mutation {
+		toggleFollow(userId: ID!): PublicUser!
+		createPost(post: PostInput!): Post!
+		togglePostLike(postId: ID!): Post!
+		login(email: String!, password: String!): Login!
+		logout(token: String!): Boolean
+	}
 `;
 
 module.exports = typeDefs;
