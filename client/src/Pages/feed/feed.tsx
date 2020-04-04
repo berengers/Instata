c6bed0691fed857a7b684014bcbff710c3f87879
Berenger Salmon <bsalmon@kaliop.com>
2020-04-04 19:11:45 +0200

    Feature - Add hook infiniteScroll

--- client/src/Pages/feed/feed.tsx
+++ client/src/Pages/feed/feed.tsx
@@ -5,25 +5,43 @@ import { useQuery } from "@apollo/react-hooks";
 import PostDetails from "Modules/post/postFeed/postDetails";
 import Sidebar from "Modules/feedSideBar/sideBarFeed";
 import Loader from "Lib/loader/loader";
+import useInfiniteScroll from "Lib/hooks/useInfiniteScroll";
 import "./feed.scss";
 
 const GET_FEED = gql`
-  query feed {
-    feed {
-      id
-      media
-      description
-      liked
-      likesCount
-      createdAt
-      user {
+  query feed($limit: Int, $cursor: FeedCursorInput) {
+    feed(limit: $limit, cursor: $cursor) {
+      cursor {
+        postDate
+        postId
+      }
+      hasMore
+      posts {
         id
-        username
-        profilePicture
+        media
+        description
+        liked
+        likesCount
+        createdAt
+        user {
+          id
+          username
+          profilePicture
+        }
       }
     }
   }
 `;
+
+interface IFetchMore {
+  feed: IFeedQueryContract;
+}
+
+export interface IFeedQueryContract {
+  cursor: number;
+  hasMore: boolean;
+  posts: [IFeedPostContract];
+}
 
 export interface IFeedPostContract {
   id: number;
@@ -42,22 +60,65 @@ export interface IFeedUserContract {
 }
 
 function Feed() {
-  const { loading, error, data } = useQuery(GET_FEED, {
-    variables: { limit: 5 },
+  const limit = 6;
+  const { data, loading, error, fetchMore } = useQuery(GET_FEED, {
+    variables: { limit },
     fetchPolicy: "cache-and-network"
+  });
+
+  async function loadMore() {
+    if (!data.feed.hasMore) return;
+
+    await fetchMore({
+      variables: {
+        cursor: {
+          postDate: data.feed.cursor.postDate,
+          postId: data.feed.cursor.postId
+        },
+        limit
+      },
+      updateQuery: (previousResult: any, { fetchMoreResult }: any) => {
+        return {
+          feed: {
+            cursor: Object.assign(
+              data.feed.cursor,
+              fetchMoreResult.feed.cursor
+            ),
+            hasMore: fetchMoreResult.feed.hasMore,
+            posts: [
+              ...previousResult.feed.posts,
+              ...fetchMoreResult.feed.posts
+            ],
+            __typename: fetchMoreResult.feed.__typename
+          }
+        };
+      }
+    });
+  }
+
+  const [ref] = useInfiniteScroll({
+    cb: loadMore,
+    observerOptions: { rootMargin: "1500px" }
   });
 
   if (error) return <div>Error</div>;
 
-  const posts: Array<IFeedPostContract> = (data && data.feed) || [];
+  const initData = { feed: { hasMore: false, cursor: {}, posts: [] } };
+  const {
+    feed: { hasMore, posts }
+  }: IFetchMore = data || initData;
 
   return (
     <div className="Feed">
       <div className="Feed-posts">
-        <Loader display={loading} />
         {posts.map(post => (
           <PostDetails post={post} key={post.id} />
         ))}
+        {posts.length > 0 && hasMore && (
+          <div ref={ref}>
+            <Loader display={loading} />
+          </div>
+        )}
       </div>
       <aside className="Feed-sidebar">
         <Sidebar />
