027bde97ee4709811c543068ab07c55d59e840e6
Berenger Salmon <bsalmon@kaliop.com>
2020-05-03 15:47:58 +0200

    Add eslint configuration to server side

--- server/datasources/instata.js
+++ server/datasources/instata.js
@@ -1,256 +1,257 @@
-const { Op } = require("sequelize");
-const { AuthenticationError, ForbiddenError } = require("apollo-server");
-const { DataSource } = require("apollo-datasource");
-const { v4: uuid } = require("uuid");
+const { Op } = require('sequelize');
+const { AuthenticationError, ForbiddenError } = require('apollo-server');
+const { DataSource } = require('apollo-datasource');
+const { v4: uuid } = require('uuid');
 
 class InstataAPI extends DataSource {
-	constructor({ models }) {
-		super();
-		this.models = models;
-	}
+  constructor({ models }) {
+    super();
+    this.models = models;
+  }
 
-	initialize(config) {
-		this.context = config.context;
-	}
+  initialize(config) {
+    this.context = config.context;
+  }
 
-	_forbiddenError() {
-		throw new ForbiddenError();
-	}
+  // eslint-disable-next-line class-methods-use-this
+  _forbiddenError() {
+    throw new ForbiddenError();
+  }
 
-	async login(email, password) {
-		const user = await this.models.User.findOne({ where: { email, password } });
+  async login(email, password) {
+    const user = await this.models.User.findOne({ where: { email, password } });
 
-		if (!user) throw new AuthenticationError();
+    if (!user) throw new AuthenticationError();
 
-		const token = uuid();
+    const token = uuid();
 
-		try {
-			await this.models.Token.create({ token, userId: user.id });
-			return token;
-		} catch (e) {
-			throw new Error(e);
-		}
-	}
+    try {
+      await this.models.Token.create({ token, userId: user.id });
+      return token;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
 
-	async logout(token) {
-		const row = await this.models.Token.findOne({ where: { token } });
+  async logout(token) {
+    const row = await this.models.Token.findOne({ where: { token } });
 
-		if (row) {
-			await row.destroy();
-		}
+    if (row) {
+      await row.destroy();
+    }
 
-		console.error("Token didnt exist");
-	}
+    console.error("Token doesn't exist");
+  }
 
-	async getFeedPosts({ limit = 10, cursor }) {
-		if (!this.context.user.id) this._forbiddenError();
+  async getFeedPosts({ limit = 10, cursor }) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		const users = await this.models.UserSubscription.findAll({
-			where: { userFollowerId: this.context.user.id }
-		});
+    const users = await this.models.UserSubscription.findAll({
+      where: { userFollowerId: this.context.user.id }
+    });
 
-		const usersId = users.map(user => user.userFollowingId);
+    const usersId = users.map(user => user.userFollowingId);
 
-		const where = cursor
-			? {
-					[Op.or]: [
-						{
-							createdAt: { [Op.lt]: new Date(cursor.postDate) },
-							userId: { [Op.or]: usersId }
-						},
-						{
-							id: { [Op.lt]: cursor.postId },
-							createdAt: new Date(cursor.postDate),
-							userId: { [Op.or]: usersId }
-						}
-					]
-			  }
-			: { userId: { [Op.or]: usersId } };
+    const where = cursor
+      ? {
+          [Op.or]: [
+            {
+              createdAt: { [Op.lt]: new Date(cursor.postDate) },
+              userId: { [Op.or]: usersId }
+            },
+            {
+              id: { [Op.lt]: cursor.postId },
+              createdAt: new Date(cursor.postDate),
+              userId: { [Op.or]: usersId }
+            }
+          ]
+        }
+      : { userId: { [Op.or]: usersId } };
 
-		return this.models.Post.findAll({
-			limit,
-			where,
-			order: [
-				["createdAt", "DESC"],
-				["id", "DESC"]
-			],
-			include: [
-				{
-					model: this.models.User
-				}
-			]
-		});
-	}
+    return this.models.Post.findAll({
+      limit,
+      where,
+      order: [
+        ['createdAt', 'DESC'],
+        ['id', 'DESC']
+      ],
+      include: [
+        {
+          model: this.models.User
+        }
+      ]
+    });
+  }
 
-	async getLikes(post) {
-		return this.models.Like.findAll({
-			where: { postId: post.id },
-			include: [{ model: this.models.User, as: "user" }]
-		});
-	}
+  async getLikes(post) {
+    return this.models.Like.findAll({
+      where: { postId: post.id },
+      include: [{ model: this.models.User, as: 'user' }]
+    });
+  }
 
-	async addPostLike(postId) {
-		if (!this.context.user.id) this._forbiddenError();
+  async addPostLike(postId) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		await this.models.Like.create({ postId, userId: this.context.user.id });
-		return this.models.Post.findOne({ where: { id: postId } });
-	}
+    await this.models.Like.create({ postId, userId: this.context.user.id });
+    return this.models.Post.findOne({ where: { id: postId } });
+  }
 
-	async deletePostLike(postId) {
-		if (!this.context.user.id) this._forbiddenError();
+  async deletePostLike(postId) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		const like = await this.models.Like.findOne({
-			where: { postId, userId: this.context.user.id }
-		});
-		await like.destroy();
-		return this.models.Post.findOne({ where: { id: postId } });
-	}
+    const like = await this.models.Like.findOne({
+      where: { postId, userId: this.context.user.id }
+    });
+    await like.destroy();
+    return this.models.Post.findOne({ where: { id: postId } });
+  }
 
-	async getLikesCount(post) {
-		return this.models.Like.count({ where: { postId: post.id } });
-	}
+  async getLikesCount(post) {
+    return this.models.Like.count({ where: { postId: post.id } });
+  }
 
-	async getUser({ id, username, email }) {
-		const params = {};
+  async getUser({ id, username, email }) {
+    const params = {};
 
-		if (id) {
-			params.id = id;
-		} else if (username) {
-			params.username = username;
-		} else if (email) {
-			params.email = email;
-		} else if (this.context.user.id) {
-			params.id = this.context.user.id;
-		} else {
-			this._forbiddenError();
-		}
+    if (id) {
+      params.id = id;
+    } else if (username) {
+      params.username = username;
+    } else if (email) {
+      params.email = email;
+    } else if (this.context.user.id) {
+      params.id = this.context.user.id;
+    } else {
+      this._forbiddenError();
+    }
 
-		const user = await this.models.User.findOne({ where: params });
+    const user = await this.models.User.findOne({ where: params });
 
-		if (!user) throw new Error("No found user");
+    if (!user) throw new Error('No found user');
 
-		return user;
-	}
+    return user;
+  }
 
-	async getPost(id) {
-		const post = await this.models.Post.findOne({
-			where: { id },
-			include: [this.models.User]
-		});
+  async getPost(id) {
+    const post = await this.models.Post.findOne({
+      where: { id },
+      include: [this.models.User]
+    });
 
-		if (!post) throw new Error("No content");
+    if (!post) throw new Error('No content');
 
-		return post;
-	}
+    return post;
+  }
 
-	async postAlreadyLiked(postId) {
-		if (!this.context.user.id) return this._forbiddenError();
+  async postAlreadyLiked(postId) {
+    if (!this.context.user.id) return this._forbiddenError();
 
-		const liked = await this.models.Like.findOne({
-			where: { postId: postId, userId: this.context.user.id }
-		});
-		return Boolean(liked);
-	}
+    const liked = await this.models.Like.findOne({
+      where: { postId, userId: this.context.user.id }
+    });
+    return Boolean(liked);
+  }
 
-	async getFollowed(userId) {
-		const res = await this.models.UserSubscription.findAll({
-			where: { userFollowerId: userId },
-			include: [{ model: this.models.User, as: "userFollowing" }]
-		});
+  async getFollowed(userId) {
+    const res = await this.models.UserSubscription.findAll({
+      where: { userFollowerId: userId },
+      include: [{ model: this.models.User, as: 'userFollowing' }]
+    });
 
-		return res.map(el => el.userFollowing);
-	}
+    return res.map(el => el.userFollowing);
+  }
 
-	async getFollowedCount(userFollowerId) {
-		return this.models.UserSubscription.count({ where: { userFollowerId } });
-	}
+  async getFollowedCount(userFollowerId) {
+    return this.models.UserSubscription.count({ where: { userFollowerId } });
+  }
 
-	async getFollowers(userFollowingId) {
-		const res = await this.models.UserSubscription.findAll({
-			where: { userFollowingId },
-			include: [{ model: this.models.User, as: "userFollower" }]
-		});
+  async getFollowers(userFollowingId) {
+    const res = await this.models.UserSubscription.findAll({
+      where: { userFollowingId },
+      include: [{ model: this.models.User, as: 'userFollower' }]
+    });
 
-		return res.map(el => el.userFollower);
-	}
+    return res.map(el => el.userFollower);
+  }
 
-	async isFollowed(userFollowingId) {
-		if (!this.context.user.id) return null;
+  async isFollowed(userFollowingId) {
+    if (!this.context.user.id) return null;
 
-		const row = await this.models.UserSubscription.findOne({
-			where: { userFollowerId: this.context.user.id, userFollowingId }
-		});
+    const row = await this.models.UserSubscription.findOne({
+      where: { userFollowerId: this.context.user.id, userFollowingId }
+    });
 
-		return Boolean(row);
-	}
+    return Boolean(row);
+  }
 
-	async getFollowersCount(userFollowingId) {
-		return this.models.UserSubscription.count({ where: { userFollowingId } });
-	}
+  async getFollowersCount(userFollowingId) {
+    return this.models.UserSubscription.count({ where: { userFollowingId } });
+  }
 
-	async getPosts(id, cursor, limit) {
-		const userId = id || this.context.user.id;
+  async getPosts(id, cursor, limit) {
+    const userId = id || this.context.user.id;
 
-		if (!userId) this._forbiddenError();
+    if (!userId) this._forbiddenError();
 
-		const where = cursor
-			? {
-					[Op.or]: [
-						{
-							userId,
-							createdAt: { [Op.lt]: new Date(cursor.postDate) }
-						},
-						{
-							id: { [Op.lt]: cursor.postId },
-							userId,
-							createdAt: new Date(cursor.postDate)
-						}
-					]
-			  }
-			: { userId };
+    const where = cursor
+      ? {
+          [Op.or]: [
+            {
+              userId,
+              createdAt: { [Op.lt]: new Date(cursor.postDate) }
+            },
+            {
+              id: { [Op.lt]: cursor.postId },
+              userId,
+              createdAt: new Date(cursor.postDate)
+            }
+          ]
+        }
+      : { userId };
 
-		return this.models.Post.findAll({
-			where,
-			limit: limit || null,
-			order: [
-				["createdAt", "DESC"],
-				["id", "DESC"]
-			],
-			include: { model: this.models.User }
-		});
-	}
+    return this.models.Post.findAll({
+      where,
+      limit: limit || null,
+      order: [
+        ['createdAt', 'DESC'],
+        ['id', 'DESC']
+      ],
+      include: { model: this.models.User }
+    });
+  }
 
-	async getPostsCount(userId) {
-		return this.models.Post.count({ where: { userId } });
-	}
+  async getPostsCount(userId) {
+    return this.models.Post.count({ where: { userId } });
+  }
 
-	async createPost(post) {
-		if (!this.context.user.id) this._forbiddenError();
+  async createPost(post) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		return this.models.Post.create({ ...post, userId: this.context.user.id });
-	}
+    return this.models.Post.create({ ...post, userId: this.context.user.id });
+  }
 
-	async addFollow(userFollowingId) {
-		if (!this.context.user.id) this._forbiddenError();
+  async addFollow(userFollowingId) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		await this.models.UserSubscription.create({
-			userFollowingId,
-			userFollowerId: this.context.user.id
-		});
+    await this.models.UserSubscription.create({
+      userFollowingId,
+      userFollowerId: this.context.user.id
+    });
 
-		return this.models.User.findOne({ where: { id: userFollowingId } });
-	}
+    return this.models.User.findOne({ where: { id: userFollowingId } });
+  }
 
-	async deleteFollow(userFollowingId) {
-		if (!this.context.user.id) this._forbiddenError();
+  async deleteFollow(userFollowingId) {
+    if (!this.context.user.id) this._forbiddenError();
 
-		const row = await this.models.UserSubscription.findOne({
-			where: { userFollowingId }
-		});
-		await row.destroy();
+    const row = await this.models.UserSubscription.findOne({
+      where: { userFollowingId }
+    });
+    await row.destroy();
 
-		return this.models.User.findOne({ where: { id: userFollowingId } });
-	}
+    return this.models.User.findOne({ where: { id: userFollowingId } });
+  }
 }
 
 module.exports = InstataAPI;
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,51 +1,51 @@
-const loremIpsum = require("lorem-ipsum").loremIpsum;
+const { loremIpsum } = require('lorem-ipsum');
 
 const loremOptions = () => ({
   count: Math.floor(Math.random() * 3 + 1),
-  units: "paragraph"
+  units: 'paragraph'
 });
 const lorem = () => loremIpsum(loremOptions());
 
-const config = require("./config");
-const db = require("./persistence/store")(config.DB);
+const config = require('./config');
+const db = require('./persistence/store')(config.DB);
 const {
   User,
   UserSubscription,
   Token,
   Post,
   Like
-} = require("./persistence/models")(db);
+} = require('./persistence/models')(db);
 
 const users = [
   {
-    email: "tom@gmail.com",
-    password: "tom_pass",
-    username: "Toto",
-    name: "Tom Pouce",
+    email: 'tom@gmail.com',
+    password: 'tom_pass',
+    username: 'Toto',
+    name: 'Tom Pouce',
     description: lorem(),
-    profilePicture: "https://picsum.photos/id/237/200/300"
+    profilePicture: 'https://picsum.photos/id/237/200/300'
   },
   {
-    email: "pierre@gmail.com",
-    password: "pierre_pass",
-    username: "Pipi",
-    name: "Pierre Boilan",
+    email: 'pierre@gmail.com',
+    password: 'pierre_pass',
+    username: 'Pipi',
+    name: 'Pierre Boilan',
     description: lorem(),
-    profilePicture: "https://picsum.photos/id/1025/200/300"
+    profilePicture: 'https://picsum.photos/id/1025/200/300'
   },
   {
-    email: "robert@gmail.com",
-    password: "robert_pass",
-    username: "Kiloulou",
-    name: "Natalia",
-    profilePicture: "https://picsum.photos/id/1026/200/300"
+    email: 'robert@gmail.com',
+    password: 'robert_pass',
+    username: 'Kiloulou',
+    name: 'Natalia',
+    profilePicture: 'https://picsum.photos/id/1026/200/300'
   },
   {
-    email: "john@gmail.com",
-    password: "john_pass",
-    username: "DODO",
-    name: "John",
-    profilePicture: "https://picsum.photos/id/1027/200/300"
+    email: 'john@gmail.com',
+    password: 'john_pass',
+    username: 'DODO',
+    name: 'John',
+    profilePicture: 'https://picsum.photos/id/1027/200/300'
   }
 ];
 
@@ -58,173 +58,173 @@ const userSubscriptions = [
 ];
 
 const authTokens = [
-  { token: "tom_token", userId: 1 },
-  { token: "pierre_token", userId: 2 },
-  { token: "robert_token", userId: 3 },
-  { token: "john_token", userId: 4 }
+  { token: 'tom_token', userId: 1 },
+  { token: 'pierre_token', userId: 2 },
+  { token: 'robert_token', userId: 3 },
+  { token: 'john_token', userId: 4 }
 ];
 
 let i = 1050;
 const posts = [
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "1"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '1'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "2"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '2'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "2"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '2'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "3"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '3'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "3"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '3'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "3"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '3'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "3"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '3'
   },
   {
     description: lorem(),
-    media: `https://picsum.photos/id/${i++}/800/800`,
-    userId: "3"
+    media: `https://picsum.photos/id/${(i += 1)}/800/800`,
+    userId: '3'
   }
 ];
 
@@ -241,6 +241,6 @@ db.sync({ force: true })
   .then(() => Post.bulkCreate(posts))
   .then(() => Like.bulkCreate(likes))
   .then(() => {
-    console.log("Fixtures inserted <------------------");
+    console.log('Fixtures inserted <------------------');
     process.exit();
   });
